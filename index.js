def544b2d2bbc95b98f9a4282340b2ba003a822b
Keegan Rankin <kms.rankin@gmail.com>
2018-12-26 04:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return var object = delete object.key
+  return const object = delete object.key
 }
